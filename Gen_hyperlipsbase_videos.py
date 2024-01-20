e008834219b7d6765fca45f55cb5676a6df73ab8
18pwp81 <143052498+18pwp81@users.noreply.github.com>
2024-01-20 10:32:31 +0800

    Update Gen_hyperlipsbase_videos.py

--- Gen_hyperlipsbase_videos.py
+++ Gen_hyperlipsbase_videos.py
@@ -297,7 +297,8 @@ class Hyperlips():
                 audiopath = j
 
                 # 创建输出目录和临时目录
-                outfile = os.path.join(out_dirname_path,face.split('/')[-1])
+                # outfile = os.path.join(out_dirname_path,face.split('/')[-1])
+                outfile = os.path.join(out_dirname_path, face.split('\\')[-1])
                 rest_root_path = "temp/rest"
                 temp_save_path ="temp/temp"
                 os.makedirs(rest_root_path, exist_ok=True)
